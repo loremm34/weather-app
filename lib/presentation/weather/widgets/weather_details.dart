1ff7bddd5ffb293120b16397c1667cc895b29b0d
Danila Yevdokimov <danila.yevdokimov@gmail.com>
2024-08-08 00:47:43 +0300

    smth

--- lib/presentation/weather/widgets/weather_details.dart
+++ lib/presentation/weather/widgets/weather_details.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:weather_app/common/widgets/basic_app_button.dart';
 
-class WeatherDetails extends StatelessWidget {
+class WeatherDetails extends ConsumerWidget {
   const WeatherDetails({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
