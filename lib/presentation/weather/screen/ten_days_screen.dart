275f015612673b8fb24404a885651fa045533d5d
Danila Yevdokimov <danila.yevdokimov@gmail.com>
2024-08-19 22:25:21 +0300

    ten days screen added

--- lib/presentation/weather/screen/ten_days_screen.dart
+++ lib/presentation/weather/screen/ten_days_screen.dart
@@ -5,6 +5,64 @@ class TenDaysScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 50),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                const Text(
+                  'Kharkiv, Ukraine',
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                ),
+                const Spacer(),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.search,
+                    size: 24,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            const Row(
+              children: [
+                Text(
+                  '3°',
+                  style: TextStyle(
+                    fontSize: 92,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(width: 10),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 15),
+                    Text(
+                      'Feels like -2',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                  ],
+                ),
+                Spacer(),
+                Icon(
+                  Icons.wb_sunny,
+                  size: 80,
+                  color: Colors.amber,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
